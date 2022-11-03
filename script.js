2240ea157d43ee1fe03878ccb35eb49acc369f0b
nika-chabu <113546606+nikachabu@users.noreply.github.com>
2022-11-03 18:08:04 +0400

    js

---
+++ script.js
@@ -0,0 +1,32 @@
+// homework-1
+function evnenNumber(number){
+    return number % 2==0;
+}
+evnenNumber()
+
+// homework-2
+
+function rectanglePerimeter(a,b){
+    return (a+b)*2;
+}
+rectanglePerimeter()
+
+// homework-3
+
+function rectanglePerimeter(a,b){
+    return(a+b)*2;
+}
+   function getSquarePerimeter(a) {
+        return rectanglePerimeter(a,a);
+}
+getSquarePerimeter()
+
+
+// homework-4
+function getRandom(){
+    return Math.floor(math.random()*100);
+}
+getRandom()
+
+
+
